1821dbf1cd634fcdc3efba6a2d4373a8d2838dea
piotr biedermann <piotr.biedermann84@gmail.com>
2023-12-24 14:12:46 +0100

    fixed the database container, the issue was with mysqld command and the config file now it should be good

--- requirements/srcs/mariadb/components/db_setup.sh
+++ requirements/srcs/mariadb/components/db_setup.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-service mysql start
+service mariadb start
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';\
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;\
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT' ;\
 FLUSH PRIVILEGES;"
-mysqld
+service mariadb stop
+sudo mysqld --user=root --bind-address=0.0.0.0
