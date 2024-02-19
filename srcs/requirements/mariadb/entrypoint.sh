6205456d1e9beaa08f6362b3d851aa726508960a
piotr biedermann <piotr.biedermann84@gmail.com>
2024-02-19 17:57:18 +0100

    removed sevice mariadb enable and added a sleep to make it work without error

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -4,9 +4,10 @@ if [ ! -e /root/.not_first_run ]; then
     touch /root/.not_first_run
     sed -i 's/bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 		service mariadb start
-		service mariadb enable
+		# service mariadb enable
     INITIALIZATION_STATEMENTS="CREATE USER 'your_user'@'172.17.0.1' IDENTIFIED BY 'your_password'; GRANT ALL PRIVILEGES ON *.* TO 'your_user'@'172.17.0.1' IDENTIFIED BY 'your_password' WITH GRANT OPTION; FLUSH PRIVILEGES;"
     mysql -u root --password="" -e "$INITIALIZATION_STATEMENTS"
+		# sleep 5
 		service mariadb stop
 fi
 exec mysqld_safe
