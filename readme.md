0ea2a07f026428355fb65fb333ff16d38f9d3adb
piotr biedermann <piotr.biedermann84@gmail.com>
2024-02-20 15:41:14 +0100

    added wordpress container, installed the cli created a network and volume, connected the volume to the wordpress

--- readme.md
+++ readme.md
@@ -1,1 +1,28 @@
 In order to run username.42.fr you need to change the /env/hosts file. Add 127.0.0.1
+Managed to get wordpress to start. Installed wordpress cli. Need to setup the /etc/php/8.2/fpm/pool.d/www.conf file. Set listen to 0.0.0.0:9000. Add more pm values.
+
+Need to create a wordpress database or upload a ready database.
+
+#create a docker volume
+docker volume create wp_files
+
+# create a docker network
+docker network create inception
+
+#Inside srcs/requirements/mariadb folder build the mariadb container
+docker build -t mariadb:00 --network=inception .
+
+#Run the mariadb container
+docker run -d --name mariadb --network=inception mariadb:00
+
+#Inside srcs/requirements/wordpress folder to build the wordpress container
+
+docker build -t wordpress:01 --network=inception .
+docker run -it --name wordpress --network=inception wordpress:01
+
+#Run with the volumes
+docker run -it --name wordpress --network=inception -v wp_files:/var/www/html wordpress:01
+
+#In the srcs/requirements/nginx folder
+docker build --network=inception -t nginx:02 .
+docker run --name nginx --network=inception -v wp_files:/var/www/html:ro -p 443:443 nginx:02
--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -5,9 +5,14 @@ if [ ! -e /root/.not_first_run ]; then
     sed -i 's/bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 		service mariadb start
 		# service mariadb enable
-    INITIALIZATION_STATEMENTS="CREATE USER 'your_user'@'172.17.0.1' IDENTIFIED BY 'your_password'; GRANT ALL PRIVILEGES ON *.* TO 'your_user'@'172.17.0.1' IDENTIFIED BY 'your_password' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-    mysql -u root --password="" -e "$INITIALIZATION_STATEMENTS"
+    # INITIALIZATION_STATEMENTS="CREATE USER 'your_user'@'172.17.0.1' IDENTIFIED BY 'your_password'; GRANT ALL PRIVILEGES ON *.* TO 'your_user'@'172.17.0.1' IDENTIFIED BY 'your_password' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+    # maybe will need to work on wordpress database
+		INITIALIZATION_STATEMENTS="CREATE USER 'your_user'@'%' IDENTIFIED BY 'your_password'; GRANT ALL PRIVILEGES ON *.* TO 'your_user'@'%' IDENTIFIED BY 'your_password' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+		mysql -u root --password="" -e "$INITIALIZATION_STATEMENTS"
 		# sleep 5
 		service mariadb stop
 fi
 exec mysqld_safe
+
+# replace with the following to connect wordpress
+# 'your_user'@'%' IDENTIFIED BY 'your_password'; GRANT ALL PRIVILEGES ON *.* TO 'your_user'@'%' IDENTIFIED BY 'your_password' WITH GRANT OPTION; FLUSH PRIVILEGES;
