4c403dfc59b64f0cee0110157ff60ab18069a27d
piotr biedermann <piotr.biedermann84@gmail.com>
2024-02-21 16:56:14 +0100

    initial commit

---
+++ srcs/requirements/wordpress/script/wordpress.sh
@@ -0,0 +1,10 @@
+if [ ! -e /root/.not_first_run ]; then
+    touch /root/.not_first_run
+		sleep 5 
+		wp core download --allow-root && \
+    wp config create --dbname=wordpress --dbuser=your_user --dbpass=your_password --dbhost=mariadb --allow-root
+		wp core install --url=pbiederm.42.fr --title=Inception --admin_user=your_user --admin_password=your_password --admin_email=pbiederm@student.42wolfsburg.de --allow-root --skip-email
+		wp plugin update --all
+		wp theme install twentysixteen --activate
+fi
+php-fpm8.2 --nodaemonize
