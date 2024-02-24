d2726cffd66ee65a173c80ce16d4a5dbf1820777
piotr biedermann <piotr.biedermann84@gmail.com>
2024-02-24 21:38:35 +0100

    added enviromental variables, hid passwords

--- srcs/requirements/wordpress/script/wordpress.sh
+++ srcs/requirements/wordpress/script/wordpress.sh
@@ -1,8 +1,8 @@
 if [ ! -f .not_first_run ]; then
     wp core download --allow-root
     wp config create --dbname=wordpress --dbuser=your_user --dbpass=your_password --dbhost=mariadb --allow-root
-    wp core install --url=pbiederm.42.fr --title=Inception --admin_user=your_user --admin_password=your_password --admin_email=pbiederm@student.42wolfsburg.de --allow-root --skip-email
-		wp user create second_user second_user@sec.com --role=author --user_pass=second_user_pass --allow-root
+    wp core install --url=$DOMAIN_NAME --title=Inception --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --skip-email
+		wp user create $SECOND_USER $SECOND_USER_EMAIL --role=author --user_pass=$SECOND_USER_PASSWORD --allow-root
     wp theme install twentysixteen --activate
     wp plugin update --all
 
